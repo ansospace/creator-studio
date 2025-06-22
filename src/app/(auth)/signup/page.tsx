4ae94f72482e4b1c0bda90c8fec4da8d219e8d56
Abhishek Gupta <abhishek.nexgen.dev@gmail.com>
2025-06-22 04:30:42 +0000

    feat(auth): add NavBar component to login and signup pages

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,33 +1,37 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import NavBar from "@/components/NavBar";
 import { Typography } from "@/components/ui";
 
 import { SignUpForm } from "../components";
 
 const LoginPage = () => {
   return (
-    <div className="h-vh container m-auto flex min-h-dvh w-svw items-center justify-center gap-10 p-6">
-      <div className="lg:1/4 w-full sm:w-2/3 md:w-2/4 lg:w-1/3">
-        <Typography variant="h2" className="border-none">
-          Want to be an
-          <Typography variant="span" className="text-primary">
-            &nbsp;Ansopedian?
+    <>
+      <NavBar />
+      <div className="h-vh container m-auto flex min-h-dvh w-svw items-center justify-center gap-10 p-6">
+        <div className="lg:1/4 w-full sm:w-2/3 md:w-2/4 lg:w-1/3">
+          <Typography variant="h2" className="border-none">
+            Want to be an
+            <Typography variant="span" className="text-primary">
+              &nbsp;Ansopedian?
+            </Typography>
           </Typography>
-        </Typography>
-        <Typography>Sign up to continue</Typography>
-        <SignUpForm />
-        <Typography className="mt-2">
-          Already have an account?
-          <Link href="/login" className="text-blue-500 hover:underline">
-            &nbsp;Login
-          </Link>
-        </Typography>
+          <Typography>Sign up to continue</Typography>
+          <SignUpForm />
+          <Typography className="mt-2">
+            Already have an account?
+            <Link href="/login" className="text-blue-500 hover:underline">
+              &nbsp;Login
+            </Link>
+          </Typography>
+        </div>
+        <div className="hidden items-center justify-center sm:block sm:w-2/3 md:w-2/3 lg:w-1/3">
+          <Image src="/images/sign-up-illustrator.svg" alt="login" width={500} height={500} priority />
+        </div>
       </div>
-      <div className="hidden items-center justify-center sm:block sm:w-2/3 md:w-2/3 lg:w-1/3">
-        <Image src="/images/sign-up-illustrator.svg" alt="login" width={500} height={500} priority />
-      </div>
-    </div>
+    </>
   );
 };
 
